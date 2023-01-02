2ef5168cc3675488db1305284a28beb9430c01f4
Crystal <crystal.ochoa03@gmail.com>
2023-01-02 21:50:08 +0100

    Java Exercises 1 & 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,18 @@
 public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    int myFavoriteNumber;
+    myFavoriteNumber = 3;
+    String myString;
+    myString = "My favorite number is odd";
+    float myNumber;
+    myNumber = 3.14F;
+    System.out.println(myString);
+    System.out.println(myNumber);
+    System.out.println(myFavoriteNumber);
+
+
+    }
+
 }
