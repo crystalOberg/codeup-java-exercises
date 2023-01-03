898759db15c85137dd2691d7a3dda65aa4c7428d
Crystal <crystal.ochoa03@gmail.com>
2023-01-03 19:24:39 +0100

    Do While

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,34 @@
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
-        int i = 5;
-        while (i < 15) {
-            System.out.println(i);
-            i++;
-        }
+//        int i = 5;
+//        while (i < 15) {
+//            System.out.println(i);
+//            i++;
+//            }
+
+//            int count = 0;
+//         do{
+//             if(count%2 == 0){
+//                 System.out.println(count);
+//             }
+//             count++;
+//         }while(count <= 100);
+
+//         int count2 = 100;
+//         do {
+//             if(count2%5 == 0){
+//             System.out.println(count2);
+//             }
+//             count2 -= 5;
+//         }while(count2 >= -10);
+
+         long count3 = 2;
+         do {
+             System.out.println(count3);
+             count3 *= count3;
+         }while(count3 < 1000000);
+
 
     }
 }
