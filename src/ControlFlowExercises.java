3ccf29c39943a6a0fb06cd39a140a70a3adf4505
Crystal <crystal.ochoa03@gmail.com>
2023-01-04 19:01:09 +0100

    Numbers to Letter Grades

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -31,6 +31,7 @@ public class ControlFlowExercises {
 //             count3 *= count3;
 //         }while(count3 < 1000000);
 
+//////////////QUESTION 2////////////////
 //        int n = 100;
 
 //        for (int i=1; i<=n; i++)
@@ -48,21 +49,51 @@ public class ControlFlowExercises {
 //                System.out.println(i);
 //        }
 
-        Scanner sorina = new Scanner(System.in);
-        int userInput = 0;
-        String playAgain = "y";
+//////////////QUESTION 3////////////////
+//        Scanner sorina = new Scanner(System.in);
+//        int userInput = 0;
+//        String playAgain = "y";
 
-        while(playAgain.equalsIgnoreCase("y")){
-            System.out.print("What number would you like to go to?");
-            userInput = sorina.nextInt();
-            System.out.println("Here is your table! \n");
-            System.out.println("number | squared | cubed");
-            System.out.println("------ | ------  | ------");
-            for(int i = 1; i <= userInput; i++){
-                System.out.printf("%s    | %s    | %s    \n", i, i*i, i*i*i);
-            }
-            System.out.println("Would you like to play again?");
-            playAgain = sorina.next();
+//        while(playAgain.equalsIgnoreCase("y")){
+//            System.out.print("What number would you like to go to?");
+//            userInput = sorina.nextInt();
+//            System.out.println("Here is your table! \n");
+//            System.out.println("number | squared | cubed");
+//            System.out.println("------ | ------  | ------");
+//            for(int i = 1; i <= userInput; i++){
+//                System.out.printf("%s    | %s    | %s    \n", i, i*i, i*i*i);
+//            }
+//            System.out.println("Would you like to play again?");
+//            playAgain = sorina.next();
+//        }
+
+        Scanner grades = new Scanner(System.in);
+        int x = 0;
+
+        while(true) {
+            System.out.println("Please enter your score to recieve your corresponding letter grade? ");
+            x = grades.nextInt();
+
+            if (x >= 0 && x <= 100)
+                break;
+
+            System.out.println("Please enter a number from 0 - 100!\n");
+        }
+
+        if (x <= 100 && x >= 96) {
+            System.out.println("A+");
+        }
+        else if (x <= 95 && x >= 92) {
+            System.out.println("A");
+        }
+        else if (x <= 91 && x >= 90) {
+            System.out.println("A-");
+        }
+        else if (x <= 89 && x >= 50) {
+            System.out.println("B+");
+        }
+        else if (x <= 49 && x >= 0) {
+            System.out.println("B");
         }
     }
 }
